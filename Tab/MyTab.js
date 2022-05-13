c2bd88c8bb333dd02e6eece86b64f0aa7e269ddb
whgus613 <whgus108@gmail.com>
2022-05-13 10:50:16 +0900

    myTab

--- Tab/MyTab.js
+++ Tab/MyTab.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
 const MyTab = () => {
   return (
     <View style={styles.container}>
-      <Text>MyTab</Text>
+      <View style={styles.profileView}>
+        <EvilIcons name="user" size={75} color="#888888" />
+        <Text style={{fontSize: 20}}>user</Text>
+      </View>
+      <View style={styles.etcView}>
+        <Text style={styles.etcTxt}>공지사항</Text>
+        <Entypo name="chevron-small-right" size={24} color="gray" />
+      </View>
+      <View style={styles.etcView}>
+        <Text style={styles.etcTxt}>이벤트</Text>
+        <Entypo name="chevron-small-right" size={24} color="gray" />
+      </View>
+      <View style={styles.etcView}>
+        <Text style={styles.etcTxt}>고객센터</Text>
+        <Entypo name="chevron-small-right" size={24} color="gray" />
+      </View>
+      <View style={styles.etcView}>
+        <Text style={styles.etcTxt}>환경설정</Text>
+        <Entypo name="chevron-small-right" size={24} color="gray" />
+      </View>
     </View>
   );
 }
@@ -13,9 +34,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 80,
+    paddingHorizontal: 30
   },
+  profileView: {
+    flexDirection: "row",
+    alignItems: 'center',
+    alignContent: 'center',
+    paddingBottom: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: "#C3C3C3",
+    marginBottom: 5
+  },
+  etcView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    borderBottomWidth: 2,
+    borderBottomColor: "#C3C3C3",
+    paddingVertical: 20
+  },
+  etcTxt: {
+    fontSize: 20
+  }
 });
 
 export default MyTab;
