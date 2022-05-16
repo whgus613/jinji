f4938d5e8955d783aa5fed0d982158698fa051b9
whgus613 <whgus108@gmail.com>
2022-05-16 11:43:06 +0900

    5/16 save 2

--- Tab/DetailFollow.js
+++ Tab/DetailFollow.js
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 const DetailFollow = () => {
   return (
     <View style={styles.container}>
-    <View style={styles.profileView}>
+      <View style={styles.profileView}>
         <View style={styles.imageView}>
           <Image
             resizeMode={"cover"}
@@ -11,12 +12,12 @@ const DetailFollow = () => {
             style={styles.imageStyle}></Image>
         </View>
         <View style={styles.followView}>
-        <View style={styles.textView}>
+          <View style={styles.textView}>
             <Text style={styles.profileTxt}>쩝쩝박사</Text>
             <View style={styles.followTag}>
               <Text style={styles.followTxt}>팔로우</Text>
             </View>
-        </View>
+          </View>
           <View style={styles.line}></View>
           <View style={styles.tagView}>
             <View style={styles.tag}>
@@ -26,13 +27,121 @@ const DetailFollow = () => {
               <Text style={styles.tagTxt}>#양식</Text>
             </View>
           </View>
-          <Text style={styles.word}>숨은 맛집 찾아다녀요~!</Text>
-          
+          <Text style={styles.word}>숨은 맛집 찾아다녀요~!</Text> 
         </View>
       </View>
-    <ScrollView>
 
-    </ScrollView>
+      <ScrollView indicatorStyle>
+
+      <View>
+        <View style={styles.storeView}>
+          <View style={styles.storeImgView}>
+            <Image 
+              resizeMode={"cover"}
+              source={require("../sampleImg/백일흑야.jpg")}
+              style={styles.storeImgStyle}></Image>
+          </View>
+          <View style={styles.storeTxtView}>
+          <View style={styles.storeTitleView}>
+            <Text style={styles.storeTitle}>백일흑야</Text>
+            <AntDesign name="hearto" size={24} color="#C7382A" />
+          </View>
+          <View style={styles.storeTagView}>
+            <View style={styles.storeTag}>
+              <Text style={styles.storeTagTxt}>#친구</Text>
+            </View>
+            <View style={styles.storeTag}>
+              <Text style={styles.storeTagTxt}>#칭따오</Text>
+            </View>
+            <View style={styles.storeTag}>
+              <Text style={styles.storeTagTxt}>#아시안</Text>
+            </View>
+           </View>
+          </View>
+        </View>
+        <View style={styles.reviewView}>
+          <Text style={styles.reviewTxt}>
+              왕치킨가스 덮밥과 쇠고기 전병이 맛있다. 세트 메뉴 적극적으로 활용하는 것도 좋을 듯! {"\n"}
+              칭따오와 함께 먹을 것을 추천!!
+          </Text>
+        </View>
+        <View style={styles.line2}></View>
+      </View>
+
+
+      <View>
+        <View style={styles.storeView}>
+          <View style={styles.storeImgView}>
+            <Image 
+              resizeMode={"cover"}
+              source={require("../sampleImg/백일흑야.jpg")}
+              style={styles.storeImgStyle}></Image>
+          </View>
+          <View style={styles.storeTxtView}>
+          <View style={styles.storeTitleView}>
+            <Text style={styles.storeTitle}>백일흑야</Text>
+            <AntDesign name="hearto" size={24} color="#C7382A" />
+          </View>
+          <View style={styles.storeTagView}>
+            <View style={styles.storeTag}>
+              <Text style={styles.storeTagTxt}>#친구</Text>
+            </View>
+            <View style={styles.storeTag}>
+              <Text style={styles.storeTagTxt}>#칭따오</Text>
+            </View>
+            <View style={styles.storeTag}>
+              <Text style={styles.storeTagTxt}>#아시안</Text>
+            </View>
+           </View>
+          </View>
+        </View>
+        <View style={styles.reviewView}>
+          <Text style={styles.reviewTxt}>
+              왕치킨가스 덮밥과 쇠고기 전병이 맛있다. 세트 메뉴 적극적으로 활용하는 것도 좋을 듯! {"\n"}
+              칭따오와 함께 먹을 것을 추천!!
+          </Text>
+        </View>
+        <View style={styles.line2}></View>
+      </View>
+
+
+      <View>
+        <View style={styles.storeView}>
+          <View style={styles.storeImgView}>
+            <Image 
+              resizeMode={"cover"}
+              source={require("../sampleImg/백일흑야.jpg")}
+              style={styles.storeImgStyle}></Image>
+          </View>
+          <View style={styles.storeTxtView}>
+          <View style={styles.storeTitleView}>
+            <Text style={styles.storeTitle}>백일흑야</Text>
+            <AntDesign name="hearto" size={24} color="#C7382A" />
+          </View>
+          <View style={styles.storeTagView}>
+            <View style={styles.storeTag}>
+              <Text style={styles.storeTagTxt}>#친구</Text>
+            </View>
+            <View style={styles.storeTag}>
+              <Text style={styles.storeTagTxt}>#칭따오</Text>
+            </View>
+            <View style={styles.storeTag}>
+              <Text style={styles.storeTagTxt}>#아시안</Text>
+            </View>
+           </View>
+          </View>
+        </View>
+        <View style={styles.reviewView}>
+          <Text style={styles.reviewTxt}>
+              왕치킨가스 덮밥과 쇠고기 전병이 맛있다. 세트 메뉴 적극적으로 활용하는 것도 좋을 듯! {"\n"}
+              칭따오와 함께 먹을 것을 추천!!
+          </Text>
+        </View>
+        <View style={styles.line2}></View>
+      </View>
+
+
+      </ScrollView>
     </View>
   );
 }
@@ -114,7 +223,59 @@ const styles = StyleSheet.create({
       followTxt: {
         color: "white"
       },
-      
+      storeView: {
+        flexDirection: 'row',
+      },
+      storeImgView: {
+        marginRight: 20
+      },
+      storeImgStyle: {
+        width: 170,
+        height: 130,
+      },
+      storeTxtView: {
+        flexDirection: 'column',
+        width: '65%'
+      },
+      storeTitleView: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+        width: '70%'
+      },
+      storeTitle: {
+        fontSize: 19
+      },
+      storeTagView: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        width: '60%'
+      },
+      storeTag: {
+        paddingHorizontal: 10,
+        borderColor: "#DA7469",
+        borderWidth: 1,
+        marginRight: 5,
+        height: 25,
+        borderRadius: 20,
+        justifyContent: 'center',
+        marginBottom: 10
+      },
+      storeTagTxt: {
+        fontSize: 13,
+        color: "#DA7469"
+      },
+      reviewView: {
+        marginVertical: 20,
+      },
+      reviewTxt: {
+        lineHeight: 20
+      },
+      line2: {
+        backgroundColor: '#C3C3C3',
+        height: 3,
+        marginBottom: 20
+      }
 });
 
 export default DetailFollow;
