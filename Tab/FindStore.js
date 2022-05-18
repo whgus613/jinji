3a9ccdb3b883ab52993ad93fbb941ffdcdc4c741
whgus613 <whgus108@gmail.com>
2022-05-18 16:32:35 +0900

    5/18 save 2

--- Tab/FindStore.js
+++ Tab/FindStore.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import { Searchbar } from 'react-native-paper';
 
 const FindStore = () => {
+
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const onChangeSearch = query => setSearchQuery(query);
+
   return (
     <View style={styles.container}>
-      <Text>FindStore</Text>
+      <Searchbar
+        placeholder='식당명, 주소 검색'
+        onChangeText = {onChangeSearch}
+        value = {searchQuery}
+        style = {{
+          marginBottom: 30,
+          borderRadius: 20
+          }}
+      />
     </View>
   );
 }
@@ -14,8 +27,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 30,
+    alignContent: 'flex-start',
+    height: '100%'
   },
 });
 
--- Tab/HomeTab.js
+++ Tab/HomeTab.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import { Searchbar } from 'react-native-paper';
 
 const HomeTab = () => {
+
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const onChangeSearch = query => setSearchQuery(query);
+
   return (
-    <View style={styles.container}>
-      <Text>HomeTab</Text>
+    <View>
+    <MapView
+      style={styles.map}
+      provide={PROVIDER_GOOGLE}
+      region={{
+        latitude: 33.45594224084678,
+        longitude: 126.56190413060305,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005
+      }}>
+      
+    </MapView>
     </View>
   );
 }
@@ -14,9 +29,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    height: "100%"
+  },
+  searchStyle:{
+    position: 'absolute'
+  }
 });
 
 export default HomeTab;
